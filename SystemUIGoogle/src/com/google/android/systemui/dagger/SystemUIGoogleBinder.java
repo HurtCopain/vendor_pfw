6357351f17e7849418a26a8cca8c9f212d696931
anayw2001 <awadhera@berkeley.edu>
2023-02-24 10:13:25 +0000

    SystemUIGoogle: Add Quick affordance and Introduce camera & flashlight keyguard affordance This adds buttons for launching camera and toggling flashlight at the bottom of the lockscreen for quick access. These will be shown only when device controls/wallet/qr scanner are disabled.

--- SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleBinder.java
+++ SystemUIGoogle/src/com/google/android/systemui/dagger/SystemUIGoogleBinder.java
@@ -16,7 +16,7 @@
 
 package com.google.android.systemui.dagger;
 
-import com.android.systemui.keyguard.dagger.KeyguardModule;
+import com.google.android.systemui.keyguard.dagger.ZephyrusKeyguardModule;
 import com.android.systemui.recents.RecentsModule;
 import com.google.android.systemui.statusbar.dagger.CentralSurfacesGoogleModule;
 import com.google.android.systemui.columbus.dagger.ColumbusBinderModule;
@@ -29,7 +29,7 @@ import dagger.Module;
 @Module(includes = {
         RecentsModule.class,
         CentralSurfacesGoogleModule.class,
-        KeyguardModule.class,
+        ZephyrusKeyguardModule.class,
         ColumbusBinderModule.class,
 })
 public abstract class SystemUIGoogleBinder {
---
+++ SystemUIGoogle/src/com/google/android/systemui/keyguard/domain/quickaffordance/ZephyrusKeyguardQuickAffordanceRegistryImpl.kt
@@ -0,0 +1,51 @@
+package com.google.android.systemui.keyguard.domain.quickaffordance
+
+import com.android.systemui.keyguard.domain.model.KeyguardQuickAffordancePosition
+import com.android.systemui.keyguard.domain.quickaffordance.HomeControlsKeyguardQuickAffordanceConfig
+import com.android.systemui.keyguard.domain.quickaffordance.KeyguardQuickAffordanceConfig
+import com.android.systemui.keyguard.domain.quickaffordance.KeyguardQuickAffordanceRegistry
+import com.android.systemui.keyguard.domain.quickaffordance.KeyguardQuickAffordanceRegistryImpl
+import com.android.systemui.keyguard.domain.quickaffordance.QrCodeScannerKeyguardQuickAffordanceConfig
+import com.android.systemui.keyguard.domain.quickaffordance.QuickAccessWalletKeyguardQuickAffordanceConfig
+
+import javax.inject.Inject
+import kotlin.reflect.KClass
+
+class ZephyrusKeyguardQuickAffordanceRegistryImpl
+@Inject
+constructor(
+    homeControls: HomeControlsKeyguardQuickAffordanceConfig,
+    quickAccessWallet: QuickAccessWalletKeyguardQuickAffordanceConfig,
+    qrCodeScanner: QrCodeScannerKeyguardQuickAffordanceConfig,
+    camera: CameraKeyguardQuickAffordanceConfig,
+    flashlight: FlashlightKeyguardQuickAffordanceConfig,
+) : KeyguardQuickAffordanceRegistry<KeyguardQuickAffordanceConfig> {
+    private val configsByPosition =
+        mapOf(
+            KeyguardQuickAffordancePosition.BOTTOM_START to
+                listOf(
+                    homeControls,
+                    flashlight
+                ),
+            KeyguardQuickAffordancePosition.BOTTOM_END to
+                listOf(
+                    quickAccessWallet,
+                    qrCodeScanner,
+                    camera
+                ),
+        )
+    private val configByClass =
+        configsByPosition.values.flatten().associateBy { config -> config::class }
+
+    override fun getAll(
+        position: KeyguardQuickAffordancePosition,
+    ): List<KeyguardQuickAffordanceConfig> {
+        return configsByPosition.getValue(position)
+    }
+
+    override fun get(
+        configClass: KClass<out KeyguardQuickAffordanceConfig>
+    ): KeyguardQuickAffordanceConfig {
+        return configByClass.getValue(configClass)
+    }
+}
